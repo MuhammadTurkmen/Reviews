f5b96e80956407e83cb4d0c01a2a31b96f75d030
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-07 11:03:02 +0430

    init

--- app.js
+++ app.js
@@ -45,11 +45,15 @@ let currentItem = 0;
 
 // load intial item 
 window.addEventListener('DOMContentLoaded', function() {
-  const item = reviews[currentItem]
+
+})
+
+// show person based on item
+
+function showPerson(person) {
+  const item = reviews[person]
   img.src = item.img
   author.textContent = item.name
   job.textContent = item.job
   info.textContent = item.text
-})
-
-// show person based on item
+}
